422edb966aee9b14d8db93ce5d307b471b9a3f6b
ChebuRawka <fxckn@mail.ru>
2025-01-19 12:53:21 +0300

    2 задание готово

--- main.go
+++ main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-var task string
+//var task string
 
 // Структура для тела запроса
 type requestBody struct {
-	Message string `json:"message"` // Поле для обработки данных из тела запроса
+	Message interface{} `json:"message"` // Поле для обработки данных из тела запроса
 }
 
 // Структура для ответа
 type responseBody struct {
-	Message string `json:"message"` // Ответ с сообщением
+	Message string `json:"message"`         // Ответ с сообщением
+	Error   string `json:"error,omitempty"` // поле для ошибок
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
 	var body requestBody // Создаем переменную body для хранения данных из тела запроса
 
 	// Проверка на пустое тело
@@ -36,12 +38,33 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Проверяем тип данных поля message
+	if message, ok := body.Message.(string); !ok {
+		http.Error(w, "Invalid data type for message", http.StatusBadRequest)
+		return
+	} else {
+		// Если тип данных правильный, можно продолжать
+		log.Printf("Message: %s", message)
+	}
+	// создаем объект Message (используя структуру из orm.go)
+	message := Message{
+		Task:   body.Message.(string), // присваиваем сообщение из запроса
+		IsDone: false,                 // можно установить значение по умолчанию
+	}
+
+	//  Сохраняем в БД
+	if err := DB.Create(&message).Error; err != nil {
+		log.Printf("Error saving message to DB:", err)
+		http.Error(w, "Failed to save message", http.StatusInternalServerError)
+		return
+	}
+
 	// Обновляем глобальную переменную task
-	task = body.Message // если все декодирование прошло успешно, мы присваиваем значение поля Message из структуры body глобал переменной task
+	//	task = body.Message // если все декодирование прошло успешно, мы присваиваем значение поля Message из структуры body глобал переменной task
 
 	// Формируем ответ
 	response := responseBody{ // создаем объект response responseBody
-		Message: fmt.Sprintf("Task updated to: %s", task),
+		Message: fmt.Sprintf("Task created: %s", message.Task),
 	}
 
 	// Отправляем успешный ответ
@@ -50,18 +73,39 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)                // кодирования объекта response в JSON и отправки его в тело ответа.
 }
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	//создание ответа
-	response := responseBody{ // создаем объект responseBody
-		Message: fmt.Sprintf("Hello, %s!", task), // значение поля Message формируется с помощью Sprintf
+func GetMessages(w http.ResponseWriter, r *http.Request) {
+	var messages []Message
+
+	//извлекаем все сообщения из бд с помощью DB.Find
+	if err := DB.Find(&messages).Error; err != nil {
+		log.Printf("Error fetching messages from database: %v", err)
+		http.Error(w, "Failed to fetch messages", http.StatusInternalServerError)
+		return
 	}
+	//создание ответа
+	//response := responseBody{ // создаем объект responseBody
+	//	Message: fmt.Sprintf("Hello, %s!", task), // значение поля Message формируется с помощью Sprintf
+	//}
+
+	//возвращаем все сообщения в JSON формате
 	w.Header().Set("Content-Type", "application/json") // тело ответа будет содержать данные в формате JSON
-	json.NewEncoder(w).Encode(response)                // отправка json ответа. Encode - функция кодирует объект в json формат и отправляет в тело ответа
+	json.NewEncoder(w).Encode(messages)                // отправка json ответа. Encode - функция кодирует объект в json формат и отправляет в тело ответа
 }
 
 func main() {
-	http.HandleFunc("/post", PostHandler)
-	http.HandleFunc("/get", GetHandler)
+	// Вызываем метод InitDB() из файла db.go
+	InitDB()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Автоматическая миграция модели Message
+	// Создает таблицу в бд для модели Message
+	if err := DB.AutoMigrate(&Message{}); err != nil {
+		log.Fatal("Auto migration failed:", err)
+	}
+
+	//маршрутизация
+	router := mux.NewRouter()
+	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+
+	http.ListenAndServe(":8080", router)
 }
